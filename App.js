c29ec3677ede89f250b5e340bd1f582723eab7dc
pascal <paschy2002@gmail.com>
2022-07-13 20:51:32 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import Moment from "moment";
+function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-export default function App() {
+  useEffect(() => {
+    fetch("http://192.168.50.19:3000/users")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  function dateFormat(variable) {
+    var dateTime = variable;
+    return Moment(dateTime).format("DD-MM-YYYY"); //basically you can do all sorts of the formatting and others
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <Text>Chargement...</Text>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+            En cours de réparation:
+          </Text>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>
+                {item.identifiant +
+                  " : " +
+                  item.modele +
+                  " : " +
+                  dateFormat(item.date_entree)}
+              </Text>
+            )}
+          />
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+            Prêts à être exportés:
+          </Text>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.name + " : " + item.email}</Text>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ queries.js
@@ -0,0 +1,77 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "api",
+  password: "root",
+  port: 5432,
+});
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM scooters ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM scooters WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { identifiant, modele } = request.body;
+
+  pool.query(
+    "INSERT INTO scooters (identifiant, modele) VALUES ($1, $2)",
+    [identifiant, modele],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.insertId}`);
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { identifiant, modele } = request.body;
+
+  pool.query(
+    "UPDATE scooters SET identifiant = $1, modele = $2 WHERE id = $3",
+    [identifiant, modele, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM scooters WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
